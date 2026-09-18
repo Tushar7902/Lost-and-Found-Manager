cf80b50a30b655fe8a725561bbbdec212003500b
Tushar <tushar.25bai11335@vitbhopal.ac.in>
2026-09-18 23:56:52 +0530

    Update Claim.java

--- src/model/Claim.java
+++ src/model/Claim.java
@@ -1,19 +1,14 @@
 package model;
-
 import java.io.Serializable;
-
 public class Claim implements Serializable {
-
     private int claimId;
     private int lostItemId;
     private int foundItemId;
     private int claimantId;
     private String status;
     private String claimDate;
-
     public Claim(int claimId, int lostItemId, int foundItemId,
                  int claimantId, String claimDate) {
-
         this.claimId = claimId;
         this.lostItemId = lostItemId;
         this.foundItemId = foundItemId;
@@ -21,39 +16,30 @@ public class Claim implements Serializable {
         this.claimDate = claimDate;
         this.status = "PENDING";
     }
-
     public int getClaimId() {
         return claimId;
     }
-
     public int getLostItemId() {
         return lostItemId;
     }
-
     public int getFoundItemId() {
         return foundItemId;
     }
-
     public int getClaimantId() {
         return claimantId;
     }
-
     public String getStatus() {
         return status;
     }
-
     public String getClaimDate() {
         return claimDate;
     }
-
     public void approveClaim() {
         status = "APPROVED";
     }
-
     public void rejectClaim() {
         status = "REJECTED";
     }
-
     @Override
     public String toString() {
         return "----- CLAIM -----\n" +
