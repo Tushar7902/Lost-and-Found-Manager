66dee1848302b3a6af33ac5549bfeed49744c352
Tushar <tushar.25bai11335@vitbhopal.ac.in>
2026-09-18 23:57:49 +0530

    Update Item.java

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,11 +1,7 @@
 package model;
-
 import java.io.Serializable;
-
 public class Item implements Serializable {
-
     private static final long serialVersionUID = 1L;
-
     protected int itemId;
     protected String name;
     protected String category;
@@ -13,11 +9,9 @@ public class Item implements Serializable {
     protected String color;
     protected String location;
     protected String date;
-
     public Item(int itemId, String name, String category,
                 String description, String color,
                 String location, String date) {
-
         this.itemId = itemId;
         this.name = name;
         this.category = category;
@@ -26,35 +20,27 @@ public class Item implements Serializable {
         this.location = location;
         this.date = date;
     }
-
     public int getItemId() {
         return itemId;
     }
-
     public String getName() {
         return name;
     }
-
     public String getCategory() {
         return category;
     }
-
     public String getDescription() {
         return description;
     }
-
     public String getColor() {
         return color;
     }
-
     public String getLocation() {
         return location;
     }
-
     public String getDate() {
         return date;
     }
-
     @Override
     public String toString() {
         return "Item ID: " + itemId +
